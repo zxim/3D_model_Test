9b6b7e61ff17ee293c2cc822e9fbc30034a27a07
심민 <tlaals44@naver.com>
2025-04-22 19:58:26 +0900

    렌더링 전

--- src/App.tsx
+++ src/App.tsx
@@ -42,16 +42,22 @@ export default function App() {
       <directionalLight position={[-5, 5, -5]} intensity={3} />
       <pointLight position={[0, 2, 0]} intensity={3} />
 
-      {/* 태양 (상단 배경) */}
+      {/* 상단 */}
       <Model path="/model/sun/scene.gltf" position={[30, 50, 0]} scale={1} />
+      <Model path="/model/bombardino_crocodilo_game_ready_3d_model/scene.gltf" position={[-10, 40, 20]} scale={1} />
+      <Model path="/model/dimorphodon/scene.gltf" position={[-40, 40, -10]} scale={10} />
 
       {/* 바닥 모델 */}
       <Model path="/model/ground/scene.gltf" position={[0, -6, 0]} scale={5} />
-
+      
       {/* 메인 캐릭터 모델들 */}
       <Model path="/model/sinoceratops/scene.gltf" position={[0, 0, 0]} scale={3} />
       <Model path="/model/big_cat/scene.gltf" position={[30, 0, 0]} scale={10} />
       <Model path="/model/ostrich/scene.gltf" position={[-30, 17, 0]} scale={5} />
+      <Model path="/model/frog/scene.gltf" position={[30, 0, 30]} scale={400} />
+      <Model path="/model/tung_tung_tung_tung_sahur/scene.gltf" position={[0, 2, 30]} scale={6} />
+      <Model path="/model/tralalero_tralala/scene.gltf" position={[-30, 5, 30]} scale={1} />
+
 
       {/* 카메라 마우스 컨트롤 */}
       <OrbitControls makeDefault target={[0, 0.5, 0]} />
